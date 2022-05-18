1dbb123e432b5e3d1edef46d73a7e3ef33d5092a
Bryan Loh <lamofacade@gmail.com>
2022-05-18 20:48:17 +0800

    Add login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import Login from "./views/Login";
+import React from "react";
+import Helmet from "react-helmet";
+import CssBaseline from "@mui/material/CssBaseline";
+
 function App() {
-  return <div>Hi</div>;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Helmet>
+        <title>Duyeet</title>
+        <meta name="description" content="More powerful than your todo list." />
+      </Helmet>
+      <Login />
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ frontend/src/views/Login.tsx
@@ -0,0 +1,7 @@
+import Page from "../components/Page";
+
+const Login: React.FC = () => {
+  return <Page title="Login">Hello World!</Page>;
+};
+
+export default Login;
